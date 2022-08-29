320a4b3bf46bc7f85fffc4c8c0b6190be14684d0
Silvan Schmidt <silvan.genius@gmail.com>
2022-08-29 17:55:05 +0200

    Add poetry

--- pkgs/starship_EXEC.sh
+++ pkgs/starship_EXEC.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v starship)" ];
+then
+    exit
+fi
+
 wget https://starship.rs/install.sh -O install_starship.sh
 chmod +x install_starship.sh
 ./install_starship.sh -y
