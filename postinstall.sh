f8a5191c02f2e8f23aed56d9d54e81be05120773
Silvan Schmidt <silvan.genius@gmail.com>
2022-09-02 16:38:09 +0200

    VSCode

--- postinstall.sh
+++ postinstall.sh
@@ -6,3 +6,9 @@ sed -i "s/{user}/$USER/" $HOME/.config/nitrogen/nitrogen.cfg
 
 # Enable ly
 sudo systemctl enable ly.service
+
+# Install vscode theme
+mkdir -p $HOME/.vscode-oss/extensions/
+git clone https://github.com/catppuccin/vscode.git $HOME/.vscode-oss/extensions/catppuccin-vsc
+mkdir -p $HOME/.config/VSCodium/User
+echo '{ "workbench.colorTheme": "Catppuccin Mocha" }' > $HOME/.config/VSCodium/User/settings.json 
